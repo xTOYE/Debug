a9e3f0282f3a7e0f064b5c27d7e9cbed7f9afef5
xTOYE <54158826+xTOYE@users.noreply.github.com>
2019-09-04 12:05:13 +1000

    pixelmapping

---
+++ Assets/Scenes/LevelGen.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class LevelGen : MonoBehaviour
+{
+    //pixel map of level
+    public Texture2D mapTexture;
+    //aray (list) pixel and prefab mappings
+    public PixelToObject[] pixelColorMappings;
+    //current pixel color to map to compare to the mappings
+    private Color pixelColor;
+
+    private void Start()
+    {
+        
+    }
+
+    void GenerateLevel()
+    {
+        //scan the hole texture and get the poitions of objects
+        for (int x = 0; x < mapTexture.width; x++)
+        {
+            for (int y = 0; y < mapTexture.height; y++)
+            {
+                GenerateObject(x, y);
+            }
+        }
+    }
+
+    void GenerateObject(int x, int y)
+    {
+        //read pixel color
+        pixelColor = mapTexture.GetPixel(x, y);
+        if(pixelColor == Color.white)
+        {
+            //do nothing
+            return;
+        }
+        foreach(PixelToObject pixelColorMapping in pixelColorMappings)
+        {
+            //scan pixelColorMappings aray for matching color
+            if(pixelColorMapping.pixelColor.Equals(pixelColor))
+            {
+                Vector2 position = new Vector2(x, y);
+                Instantiate(pixelColorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/PixelToObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+[System.Serializable]//allows us to veiw in the inspector
+
+public class PixelToObject
+{
+    public Color pixelColor;
+    public GameObject prefab;
+}
